b733cc0e6d60e8890e6dc38d9652877a8fd54ae1
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-04-07 02:11:05 +0200

    determine location when clicking button

--- iWasWhere/ViewController.swift
+++ iWasWhere/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    // from http://szulctomasz.com/ios-9-getting-single-location-update-with-requestlocation/
+    private var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func geoRecord(sender: AnyObject) {
+        print("record")
+        locationManager.requestLocation()
+    }
+    
+    // MARK: CLLocationManagerDelegate
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error)
+    }
 
 }
 
