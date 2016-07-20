1d6820e8d87e9135ef92529984c6f0091e451e25
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-07-20 18:54:11 +0200

    added alamofire

--- Pods/Target Support Files/Pods-iWasWhere/Pods-iWasWhere-frameworks.sh
+++ Pods/Target Support Files/Pods-iWasWhere/Pods-iWasWhere-frameworks.sh
@@ -84,10 +84,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ObjectMapper/ObjectMapper.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RSBarcodes_Swift/RSBarcodes_Swift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ObjectMapper/ObjectMapper.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RSBarcodes_Swift/RSBarcodes_Swift.framework"
 fi
