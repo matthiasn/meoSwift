90c4373fcd5831c0a942f6ea8a9b0319c27002b4
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2018-03-07 21:43:19 +0100

    repair

--- Podfile
+++ Podfile
@@ -3,8 +3,8 @@ platform :ios, '11.0'
 # Uncomment this line if you're using Swift
 use_frameworks!
 
-target 'meo-iOS' do
-  pod 'ObjectMapper'
+target 'meoSwift' do
+  pod 'ObjectMapper', '~> 3.1'
   pod 'ImagePicker'
   pod 'BarcodeScanner'
 end
--- Pods/Target Support Files/Pods-meoSwift/Pods-meoSwift-umbrella.h
+++ Pods/Target Support Files/Pods-meoSwift/Pods-meoSwift-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_meo_iOSVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_meo_iOSVersionString[];
+FOUNDATION_EXPORT double Pods_meoSwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_meoSwiftVersionString[];
 
